a2deb388054bd6b84b335432a1bf3757fc86dc05
SnoPants <drewnimaticanim@gmail.com>
2026-02-19 22:56:10 -0800

    finished adding color swapping feature, added dictionarry to json.

--- maya/python/pipe/library/rigging/rig_type/builders/ds_controller_builder.py
+++ maya/python/pipe/library/rigging/rig_type/builders/ds_controller_builder.py
@@ -4,34 +4,34 @@ import json
 
 # need to figure out a better algorithm for scaling based on distance
 class RigCtrl:
-    def __init__(self, name = "NULL", scale= 1.0, translate=[0,0,0], rotation=[0,0,0]):
+    def __init__(self, name = "NULL", scale= 1.0, translate=[0,0,0], rotation=[0,0,0], color=None, shape='circle'):
         self.name = name
         self.scale = scale
         self.translate = translate
         self.rotation = rotation
-        self.color = None
-        self.shape = None
+        self.color = color
+        self.shape = shape
 
     # Left off here. (Below) - need to add the shape creation and color assignment.
     #TODO: Need to create a JSON or set of py files containing the shape data for the various control types. This will allow us to easily create different types of controls based on the shape data.
 
-    def _create_nurbs_ctrl(self):
+    def _load_nurbs_data(self):
+        global _NURBS_DATA
+        _NURBS_DATA = None
 
-        def _load_nurbs_data():
-            global _NURBS_DATA
-            _NURBS_DATA = None
-
-            if _NURBS_DATA:
-                return _NURBS_DATA
-
-            json_path = os.path.join(os.path.dirname(__file__), "ds_nurbs_curves.json")
-
-            with open(json_path, "r") as f:
-                _NURBS_DATA = json.load(f)
-
+        if _NURBS_DATA:
             return _NURBS_DATA
+
+        json_path = os.path.join(os.path.dirname(__file__), "ds_nurbs_curves.json")
+
+        with open(json_path, "r") as f:
+            _NURBS_DATA = json.load(f)
+
+        return _NURBS_DATA
+    
+    def _create_nurbs_ctrl(self):
         
-        data = _load_nurbs_data()
+        data = self._load_nurbs_data()
         #  Debug for json loading
         print(data)
         print('nurbs controls: ', data["controls"]["sphere"])
@@ -56,8 +56,13 @@ class RigCtrl:
             cmds.parent(shape_node, ctrl, shape=True, relative=True)
             cmds.delete(nurbs_curve)
 
+        print('nurbs colors: ', data["colors"])
+
+        if self.color:
+            self.set_color_shape(ctrl)
         # left off here. need to create curve based on _SHAPE_DATA
         return ctrl
+    
 
     def _create_loc(self):
         loc = cmds.spaceLocator(n= f"{self.name}_loc_01")
@@ -71,19 +76,32 @@ class RigCtrl:
 
         return loc[0], loc_grp
     
-    def create_ctrl(self, shape="circle", color=None):
+    '''def create_ctrl(self, shape="circle", color=None):
         self.shape = shape
         self.color = color
         
         ctrl = self._create_nurbs_ctrl()
 
         if color:
-            self._set_color_shape(ctrl)
+            shapes = cmds.listRelatives(ctrl, shapes=True)
+            self.set_color_shape(shapes)
 
-        return ctrl
+        return ctrl'''
+    
 
-    def _set_color_shape(self, shape):
+    def set_color_shape(self, ctrl):
+        shapes = cmds.listRelatives(ctrl, shapes=True)
+        data = self._load_nurbs_data()
+        if self.color not in data["colors"]:
+            raise ValueError(f"Color '{self.color}' not found in nurbs data.")
+        
+        color_data = data["colors"][self.color]
+        print(f"color data is {color_data}")
+        for shape in shapes:
+            cmds.setAttr(f"{shape}.overrideEnabled", 1)
+            cmds.setAttr(f"{shape}.overrideColor", color_data)
         return
+    
 
     def replace_with_new_nurbs(self, targets):
 
@@ -102,3 +120,5 @@ class RigCtrl:
             
             cmds.delete(trg_shape)
             return
+
+# Need to consider how in create_ctrl, the ctrl is placed. Do we bother giving an xform argument in it?
